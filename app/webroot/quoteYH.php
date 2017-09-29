0d70b859782b70f92b3f79a593b0253997c214c7
Richard Lee <zh.richard.lee@gmail.com>
2017-09-28 23:52:36 -0400

    Add Yahoo PHP file

--- app/webroot/quoteYH.php
+++ app/webroot/quoteYH.php
@@ -5,14 +5,16 @@
 	
 	$curl = curl_init();
 	$url = sprintf("%s%s%s", "http://query.yahooapis.com/v1/public/yql?q=select%20symbol,LastTradePriceOnly,StockExchange%20from%20yahoo.finance.quote%20where%20symbol%20in%20(", $p, ")&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
-	//echo $url;
+	echo $url;
+	echo "=============================";
 
 
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
     $result = curl_exec($curl);
-    //echo $result;
+    echo $result;
+	echo "=============================";
 
     curl_close($curl);
     $payload = "";
@@ -25,7 +27,8 @@
 		$idx2 = strpos($result, $endTag, $idx);
 		if ($idx2 >= 0 ){
 			$payload = substr($result, $idx, $idx2 + 1 - $idx);
-			//echo $payload;
+			echo $payload;
+			echo "=============================";
 			if ($startTag === "{") {
 				$payload = "[$payload]";
 			}
